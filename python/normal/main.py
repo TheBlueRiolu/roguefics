1c458c391455f2d5cdcf9352c5c65a7a580638af
Luka J. Riolu <67442042+TheBlueRiolu@users.noreply.github.com>
2024-12-24 10:25:51 -0600

    Add files via upload
    Trying to make title screen work

--- python/normal/main.py
+++ python/normal/main.py
@@ -10,23 +10,34 @@ TOP_MENU_STATE = 1
 DARK_TEXT_STATE = 2
 QUIZ_STATE = 3
 MAP_STATE = 4
+ERROR_STATE = -1
 
 def update(event: tcod.event.KeyboardEvent) -> None:
+    global state
     for i in entities:
         self.update()
+    if state == START_STATE:
+        state = TOP_MENU_STATE
 
-def draw() -> None:
-    pass
+def draw(context) -> None:
+    global state
+    if state == START_STATE:
+        with open("resources/title-screen.txt", 'r') as ts:
+            console.clear()
+            console.print(x=0, y=0, string=ts.read())
+            context.present(console)
 
 # Global variables
 state = START_STATE
 entities = []
 current_map = None
+console = None
 
 def main() -> None:
+    global console
     # Load the font
     tileset = tcod.tileset.load_tilesheet(
-        "ibmfont.png", 32, 8, tcod.tileset.CHARMAP_CP437,
+        "resources/ibmfont.png", 32, 8, tcod.tileset.CHARMAP_CP437,
     )
     # Create the main console.
     console = tcod.console.Console(SCREEN_WIDTH, SCREEN_HEIGHT, order="F")
@@ -34,10 +45,7 @@ def main() -> None:
     with tcod.context.new(  # New window for a console of size columns×rows.
         columns=console.width, rows=console.height, tileset=tileset,
     ) as context:
-        console.clear()
-        console.print(x=0, y=0, string="Hello, World!")
-        context.present(console)
-        draw()
+        draw(context)
         while True:  # Main loop, runs until SystemExit is raised.
             console.clear()
             context.present(console)
@@ -50,7 +58,7 @@ def main() -> None:
                     continue
                 print(event) # Print event names and attributes.
                 update(event)
-                draw()
+                draw(context)
                 if isinstance(event, tcod.event.Quit):
                     raise SystemExit()
         # The window will be closed after the above with-block exits.
